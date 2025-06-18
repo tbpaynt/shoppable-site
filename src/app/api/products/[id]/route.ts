281118ed52b6d8d76bb5be3dc18a931ed55b46d1
thomas paynter <ktwholesalefinds@gmail.com>
2025-06-18 00:54:19 -0400

    remove unused nextrequest import to fix eslint error

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,6 +1,5 @@
 import { NextResponse } from "next/server";
 import { createClient } from "@supabase/supabase-js";
-import type { NextRequest } from "next/server";
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
